0143c4e647b21936b7e0899c18966bb2c10aa495
joaocellbit <joaomarciopradosilva@gmail.com>
2024-12-15 14:35:34 -0300

    zoom in

--- Scripts/mundo_proc.gd
+++ Scripts/mundo_proc.gd
@@ -15,9 +15,11 @@ var water_atlas = Vector2i(14,11)
 var land_atlas = Vector2i(9,19)
 var sand_atlas = Vector2i(12,3)
 var hill_atlas = Vector2i(14,2)
+var seed
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	noise_height.noise.set_seed(-624211235)
+	noise_height.noise.set_seed(randi())
+	seed = noise_height.noise.get_seed()
 	print("seed: ", noise_height.noise.get_seed())
 	noise = noise_height.noise 
 	generate_world()
@@ -46,3 +48,21 @@ func generate_world():
 			else:
 				hill.modulate = Color(0.545, 0.271, 0.075, 1)
 				hill.set_cell(Vector2(x,y), source_id, hill_atlas)
+func _unhandled_input(event: InputEvent) -> void:
+	if Input.is_key_pressed(KEY_K):
+		noise_height.noise.frequency = 0.006
+		var PC = $"../PC"
+		print(PC.position)
+		var cordsx= PC.position.x/5
+		var cordsy= PC.position.y/5
+		var newoff = Vector3(cordsx,cordsy,0)
+		print(cordsx,cordsy)
+		print(newoff)
+		noise_height.noise.offset = newoff
+		grass.clear()
+		water.clear()
+		sand.clear()
+		hill.clear()
+		generate_world()
+		
+	 
